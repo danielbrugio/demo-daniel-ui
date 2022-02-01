40764cd529a397766dd19f8145a1a19cd36bca4b
danielbrugio <daniel.brugioni@hotmail.com>
2022-02-01 20:24:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Autocomplete, Button, Select, TextInput, NumberInput } from 'react-daniel-ui';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Button>Click Here</Button>
+    <Select/>
+    <NumberInput/>
+    <Autocomplete variant='secondary' placeholder='Autocomplete'/>
+    <TextInput variant='secondary' placeholder='Text Input'/>
+  </div>
+)
 
-export default App;
+export default App
