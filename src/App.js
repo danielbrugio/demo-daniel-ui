62e569d096f32852cdef871b2c3b09fe1b3b0289
Daniel Brugioni <65047232+danielbrugio@users.noreply.github.com>
2022-02-02 11:37:30 -0300

    appjs & appscss added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
 import React from 'react'
-import { Autocomplete, Button, Select, TextInput, NumberInput } from 'react-daniel-ui';
+import './app.scss'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
+import { Navbar } from './components/Navbar'
+import { ButtonDemo } from './components/ButtonDemo'
+import { GettingStarted } from './components/GettingStarted'
+import { TextInputDemo } from './components/TextInputDemo'
+import { NumberInputDemo } from './components/NumberInputDemo'
+import { SelectDemo } from './components/SelectDemo'
+import { AutocompleteDemo } from './components/AutocompleteDemo'
+import { Theming } from './components/Theming'
+import { TextAreaDemo } from './components/TextAreaDemo'
+import { useSelector } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
+import { DividerDemo } from './components/DividerDemo'
 
-const App = () => (
-  <div>
-    <Button>Click Here</Button>
-    <Select/>
-    <NumberInput/>
-    <Autocomplete variant='secondary' placeholder='Autocomplete'/>
-    <TextInput variant='secondary' placeholder='Text Input'/>
-  </div>
-)
+function App() {
+  const theme = useSelector((state) => state)
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className='app'>
+        <Router>
+          <Navbar />
+          <div className='main'>
+            <Switch>
+              <Route exact path='/'>
+                <Redirect to='/gettingstarted' />
+              </Route>
+              <Route path='/button' element={ButtonDemo} />
+              <Route path='/gettingstarted' element={GettingStarted} />
+              <Route path='/textinput' element={TextInputDemo} />
+              <Route path='/numberinput' element={NumberInputDemo} />
+              <Route path='/select' element={SelectDemo} />
+              <Route path='/autocomplete' element={AutocompleteDemo} />
+              <Route path='/theming' element={Theming} />
+              <Route path='/divider' element={DividerDemo} />
+              <Route path='/textarea' element={TextAreaDemo} />
+              <Route />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </ThemeProvider>
+  )
+}
 
 export default App
