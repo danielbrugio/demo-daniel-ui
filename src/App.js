7204f2bf9be33d2d330a2be1d190f173a493ae60
Daniel Brugioni <65047232+danielbrugio@users.noreply.github.com>
2022-02-04 11:34:39 -0300

    css added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./app.scss";
+import "./app.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { Theming } from "./components/Theming";
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,53 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import { Button, mountain, sea } from 'react-daniel-ui'
-import { useDispatch } from 'react-redux'
-import { changeThemeAction } from '../../redux/actions/changeThemeAction'
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import { Button, mountain, sea } from "react-daniel-ui";
+import { useDispatch } from "react-redux";
+import { changeThemeAction } from "../../redux/actions/changeThemeAction";
 
 export const Header = ({ title, changeTag = () => {}, hideTags = false }) => {
-  const [tag, setTag] = useState('useCases')
-  const dispatch = useDispatch()
+  const [tag, setTag] = useState("useCases");
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    changeTag(tag)
-  }, [tag, changeTag])
+    changeTag(tag);
+  }, [tag, changeTag]);
 
   const handleTheme = (theme) => {
-    if (theme === 'Sea') {
-      dispatch(changeThemeAction(sea))
-    } else if (theme === 'Mountain') {
-      dispatch(changeThemeAction(mountain))
+    if (theme === "Sea") {
+      dispatch(changeThemeAction(sea));
+    } else if (theme === "Mountain") {
+      dispatch(changeThemeAction(mountain));
     }
-  }
+  };
 
   return (
-    <div className='header'>
+    <div className="header">
       <h2>{title}</h2>
       {!hideTags && (
-        <div className='header__tags'>
-          <div className='header__tags--left'>
+        <div className="header__tags">
+          <div className="header__tags--left">
             <button
-              onClick={() => setTag('general')}
-              className={tag === 'general' ? 'tagselect' : 'tagunselect'}
+              onClick={() => setTag("general")}
+              className={tag === "general" ? "tagselect" : "tagunselect"}
             >
               General
             </button>
             <button
-              onClick={() => setTag('useCases')}
-              className={tag === 'useCases' ? 'tagselect' : 'tagunselect'}
+              onClick={() => setTag("useCases")}
+              className={tag === "useCases" ? "tagselect" : "tagunselect"}
             >
               Use Cases
             </button>
           </div>
-          {tag === 'useCases' && (
-            <div className='header__tags--right'>
+          {tag === "useCases" && (
+            <div className="header__tags--right">
               <h3>Change Theme</h3>
-              <Button onClick={() => handleTheme('Sea')} theme={sea}>
+              <Button onClick={() => handleTheme("Sea")} theme={sea}>
                 Sea
               </Button>
-              <Button
-                onClick={() => handleTheme('Mountain')}
-                theme={mountain}
-              >
+              <Button onClick={() => handleTheme("Mountain")} theme={mountain}>
                 Mountain
               </Button>
             </div>
@@ -55,5 +53,5 @@ export const Header = ({ title, changeTag = () => {}, hideTags = false }) => {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
